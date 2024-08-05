68481b5eb9dc5c2cb0928da1b6ec0da879c30d0c
VikasGoudar <avikashasappeared@gmail.com>
2024-08-05 20:19:58 +0530

    m

--- sliding_window/lec_1/largest_substring_without_repeating_characters.cpp
+++ sliding_window/lec_1/largest_substring_without_repeating_characters.cpp
@@ -1,1 +1,64 @@
+/*
+	https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+*/
 
+#include <iostream>
+#include <unordered_set> 
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+
+// method - using set
+int methodOne(std::string s){
+	int left = 0;
+	int length = s.length();
+	int maxLength = 0;
+
+	std::unordered_set<char> charSet;
+
+	for (int right = 0; right < length; right++){
+		if (charSet.count(s[right]) == 0){
+			charSet.insert(s[right]);
+			maxLength = std::max(maxLength, right-left+1);
+		}
+		else{
+			while (charSet.count(s[right])){
+				charSet.erase(s[left]);
+				left++;
+			}
+			charSet.insert(s[right]);
+		}
+	}
+	return maxLength;
+}
+
+// method - using map
+int methodTwo(std::string s){
+	int length = s.length();
+	int left = 0;
+	int maxLength = 0;
+
+	std::unordered_map<char,int> charMap;
+
+	for (int right = 0; right < length; right++){
+		if (charMap.count(s[right]) == 0 || charMap[s[right]] < left){
+			charMap[s[right]] = right;
+			maxLength = std::max(maxLength, right-left+1);
+		}
+		else{
+			left = charMap[s[right]] + 1;
+			charMap[s[right]] = right;
+		}
+	}
+	return maxLength;
+}
+
+int main(){
+	std::string s;
+	std::cin >> s;
+
+	std::cout << methodOne(s) << std::endl;
+	std::cout << methodTwo(s);
+	
+	return 0;
+}
